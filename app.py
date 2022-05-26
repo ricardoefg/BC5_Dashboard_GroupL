71bcb22a7cf9af96284e773423b0c2e7900e6f59
ricardoefg <90785203+ricardoefg@users.noreply.github.com>
2022-05-26 13:09:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -775,6 +775,8 @@ cards_overall = html.Div([row_1, row_2])
 app = dash.Dash(__name__, external_stylesheets=[
                 dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
+server = app.server
+
 app.layout = html.Div([
     dcc.Location(id="url"),
     sidebar,
